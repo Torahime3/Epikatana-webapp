aeedae25fc17593fd10c990db869b7f1faacce4e
VagabondSEC <ctfmail0@gmail.com>
2024-04-23 17:45:08 +0200

    fix errors conflicts and routes to products

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,19 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-<<<<<<< Updated upstream
 import Header from './components/Header';
-import Home from './components/Home';
-import Products from './components/Products';
-import Authentication from './components/Authentication';
-import Business from './components/Business';
-import './App.css';
-=======
-import Header from './pages/Header';
 import Home from './pages/Home';
 import Products from './pages/Products';
-import Connection from './pages/Connection';
-import Inscription from './pages/Inscription';
-import Nous from './pages/Nous';
->>>>>>> Stashed changes
+import Authentication from './components/Authentication';
+import { Business } from '@mui/icons-material';
+
 
 function App() {
   return (
