4aa172eaca2fbeba6b5cd2a1947b45ed31a2f7ea
Nathan Dulac <nathan.dulac@epitech.eu>
2024-05-01 12:20:37 +0200

    fix(App.css, Profile.tsx): Change background color + fix deconnexion button (added ReactFragment)

--- front/src/pages/Profile.tsx
+++ front/src/pages/Profile.tsx
@@ -6,20 +6,24 @@ const Profile = () => {
     const [cookies, setCookie, removeCookie] = useCookies(['userToken']);
 
     const handleLogout = () => {
-        // Remove the userToken cookie
         removeCookie('userToken');
-        // Redirect to the login page
         window.location.href = '/login';
       };
 
     let { data: user, isLoading, error } = useMe(cookies.userToken);
 
-    if (isLoading) return <div>Loading...</div> && <div className="profile_logout">
-    <button onClick={handleLogout}>Se déconnecter</button>
-</div>;
-    if (error) return <div>An error occurred: {error.message}</div> && <div className="profile_logout">
-    <button onClick={handleLogout}>Se déconnecter</button>
-</div>;
+    if (isLoading) return (
+            <div>Loading...</div>       
+    )
+
+    if (error) return (
+        <>
+            <div>An error occurred: {error.message}</div>
+            <div className="profile_logout">
+                <button onClick={handleLogout}>Se déconnecter</button>
+            </div>;
+        </>
+    )
 
     if(user === undefined){
         return (
