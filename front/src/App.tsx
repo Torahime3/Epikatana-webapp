558e4ed2da8a6f60c29abe0ba6848e6ee6ab6243
Steven Dorion <112935133+Noiirod@users.noreply.github.com>
2024-05-06 16:01:36 +0200

    feat: change the homepage, create a footer component

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import Products from './pages/Products';
 import Orders from './pages/Orders';
@@ -28,6 +29,7 @@ function App() {
           <Route path="/products/:id" element={<ProductDetailsPage />} />
           <Route path="/carts" element={<Carts />} />
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,16 +1,49 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Product from '../components/Product';
+import { useProducts } from '../hooks/fetchProducts';
+import "../styles/Home.css";
+
 const HomePage: React.FC = () => {
+  const { data: products, isLoading, error } = useProducts();
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+  };
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>An error occurred: {error.message}</div>;
+
   return (
     <div className="home-page">
       <header>
-        <h1>Bienvenuqsdsd ite web !</h1>
+        <h1>Bienvenue sur Epikatana, la référence du katana</h1>
       </header>
       <main>
-        <p> test site </p>
-        <p>Commence</p>
+        <p>Bienvenue sur Epikatana, votre destination ultime pour trouver le katana parfait. Explorez notre boutique et plongez dans notre vaste collection de pièces uniques et authentiques. Que vous soyez un amateur passionné d'arts martiaux ou un collectionneur chevronné, vous trouverez sûrement votre perle rare ici.</p>
+        <div className="slider">
+          <Slider {...settings}>
+            {products.map((product) => (
+              <Product
+                key={product.id}
+                id={product.id}
+                name={product.name}
+                description={product.description}
+                price={product.price}
+                photo={product.photo}
+              />
+            ))}
+          </Slider>
+        </div>
       </main>
-      <footer>
-        <p>&copy; katana stylé 2024</p>
-      </footer>
     </div>
   );
 };
